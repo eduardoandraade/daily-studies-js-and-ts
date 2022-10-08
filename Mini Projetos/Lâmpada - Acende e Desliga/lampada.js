19b1633533c85f36f4e3808fb46e702cc9086303
Eduardo Andrade <eduardoandraade@outlook.com>
2022-10-08 07:21:53 -0300

    deixando apenas um botão

--- Mini Projetos/Lâmpada - Acende e Desliga/lampada.js
+++ Mini Projetos/Lâmpada - Acende e Desliga/lampada.js
@@ -1,5 +1,4 @@
-const turnOn = document.getElementById ( 'turnOn' );
-const turnOff = document.getElementById ( 'turnOff' );
+const turnOnOff = document.getElementById ( 'turnOn' );
 const lamp = document.getElementById ( 'lamp' );
 
 function isLampBroken () {
@@ -23,9 +22,18 @@ function lampBroken () {
 }
 
 
+function lampOnOff () {
+    if ( turnOnOff.textContent == 'Ligar' ) {
+        lampOn();
+        turnOnOff.textContent = 'Desligar';
+    }else{ 
+        lampOff();
+        turnOnOff.textContent = 'Ligar';
+    }
+}
 
-turnOn.addEventListener ( 'click', lampOn );
-turnOff.addEventListener ( 'click', lampOff );
+turnOnOff.addEventListener ( 'click', lampOnOff );
+
 lamp.addEventListener ( 'mouseover', lampOn );
 lamp.addEventListener ( 'mouseleave', lampOff );
 lamp.addEventListener ( 'dblclick', lampBroken );
