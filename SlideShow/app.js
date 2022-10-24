560707aa6410500c89ecd3d3bb6891b4e34699d6
Eduardo Andrade <eduardoandraade@outlook.com>
2022-10-24 20:08:32 -0300

    Imagens funcionando
    Button pronto e tamanho do slide pronto também.
    
    faltando as funcionalidades do slide

--- SlideShow/app.js
+++ SlideShow/app.js
@@ -4,20 +4,19 @@ const images = [
     { 'id': '1', 'url':'./img/1.jpg' },
     { 'id': '2', 'url':'./img/2.jpg' },
     { 'id': '3', 'url':'./img/3.jpg' },
-    { 'id': '4', 'url':'./img/ligada.jpg' },
+    { 'id': '4', 'url':'./img/noite.svg' },
 ]
-
 
 const container = document.querySelector('#container-items');
 
 const loadImages = ( images, container ) => {
-    images.forEach( image => {
+    images.forEach ( image => {
         container.innerHTML += `
             <div class='item'>
-                <img src='${images.url}'
+                <img src='${image.url}'
             </div>
         `
-    });
+    })
 }
 
 loadImages ( images, container );
