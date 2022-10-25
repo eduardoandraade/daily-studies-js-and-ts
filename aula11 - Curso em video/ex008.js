f78ef9ef3c0a87f6d2841d23a64410d69925556c
Eduardo Andrade <eduardoandraade@outlook.com>
2022-10-25 07:43:47 -0300

    Condições simples
    Entendendo condições simples

---
+++ aula11 - Curso em video/ex008.js
@@ -0,0 +1,8 @@
+var vel = 5
+console.log (`A velocidade do seu carro é ${vel}KM/h`)
+
+if (vel > 60) {
+    //condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Diriga sempre usando o cinto de segurança.`)
