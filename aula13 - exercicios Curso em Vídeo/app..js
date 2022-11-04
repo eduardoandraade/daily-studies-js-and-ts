fb4150c488cf91b068f5b7a887152f2fb8a4a186
Eduardo Andrade <eduardoandraade@outlook.com>
2022-11-04 07:34:16 -0300

    inserindo imagem no input

--- aula13 - exercicios Curso em Vídeo/app..js
+++ aula13 - exercicios Curso em Vídeo/app..js
@@ -1,19 +1,49 @@
 function verificar() {
-    var data = new Date();
-    var ano = data.getFullYear();
-    var fano = document.getElementById('txtano');
-    var res = document.querySelector('dvi#res');
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
     if (fano.value.length == 0 || Number (fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
-            gênero = 'Homem'
+            gênero = 'homem'
+            if (idade >=0 && idade < 10) {
+                // criança
+                img.setAttribute('src', './img/bebe-m.png')
+            } else if (idade <21) {
+                // jovem
+                img.setAttribute('src', './img/jovem-m.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', './img/adulto-m.png')
+            } else {
+                //idoso
+                img.setAttribute('src', './img/idoso-m.png')
+            }
         } else if (fsex[1].checked) {
-            gênero = 'Mulher'
+            gênero = 'mulher'
+            if (idade >=0 && idade < 10) {
+                // criança
+                img.setAttribute('src', './img/bebe-f.png')
+            } else if (idade <21) {
+                // jovem
+                img.setAttribute('src', './img/jovem-f.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', './img/adulto-f.png')
+            } else {
+                //idoso
+                img.setAttribute('src', './img/idoso-f.png')
+            }
         }
-        res.innerHTML = ``Detectamos ${gênero} com ${idade} anos.`
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
