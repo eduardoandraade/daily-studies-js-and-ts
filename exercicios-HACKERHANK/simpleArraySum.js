ecbadc4cfb812f844fe35b4c6dc97e8123aa67ca
Eduardo Andrade <109284750+eduardoandraade@users.noreply.github.com>
2023-03-27 15:29:00 -0300

    iterando arrays bidimensionais

---
+++ exercicios-HACKERHANK/simpleArraySum.js
@@ -0,0 +1,8 @@
+function simplesArraySum(ar) {
+    let soma = 0;
+    for(let i = 0; i < ar.length; i++) {
+        soma = soma + ar[i]
+    }
+    return soma;
+};
+
---
+++ exercicios.OBC/matrizes/matrizes.js
@@ -0,0 +1,37 @@
+const arr = [
+   "1º Nível",
+   ["2º Nível", 42, true],
+   [
+    ["3º Nível, 1º item", "Olá, mundo!"],
+    ["3º Nível, 2º item", "Hello, World!"]
+   ],
+   []
+]
+
+console.log(arr)
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[1] [0])
+console.log(arr[1] [1])
+console.log(arr[2])
+console.log(arr[2] [1])
+console.log(arr[2] [1] [0])
+
+const matriz = [
+    ["l1, c1", "l1, c2", "l1, c3", "l1, c4"],
+    ["l2, c1", "l2, c2", "l2, c3", "l2, c4"],
+    ["l3, c1", "l3, c2", "l3, c3", "l3, c4"]
+]
+
+
+
+// matriz.push(["Nova linha"])
+// matriz[0].push("Nova coluna")
+
+for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+        const elemento = matriz[i][j]
+        console.log("Posição: (" + i + ", " + j + ") Valor: " + elemento)
+    }
+    console.log(matriz[i])
+}
