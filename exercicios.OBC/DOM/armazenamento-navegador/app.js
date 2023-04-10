54200246a104190c0d5b4cf7c4b5a1be16bb1c98
Eduardo Andrade <109284750+eduardoandraade@users.noreply.github.com>
2023-04-10 13:18:19 -0300

    Data Storage in the Broswer
    practice using Session Storage, Local Storage e Cookies

---
+++ exercicios.OBC/DOM/armazenamento-navegador/app.js
@@ -0,0 +1,44 @@
+document.getElementById('sessionBtn').addEventListener('click', function () {
+    const input = document.getElementById('session')
+    sessionStorage.setItem('info', input.value)
+    input.value = ''
+})
+
+document.getElementById('readSesssion').addEventListener('click', function () {
+    const info = sessionStorage.getItem('info')
+    alert('A informação salva é: ' + info)
+})
+
+document.getElementById('localBtn').addEventListener('click', function () {
+    const input = document.getElementById('local')
+    localStorage.setItem('text', input.value)
+    input.value = ''
+})
+
+document.getElementById('readLocal').addEventListener('click', function () {
+    const t = localStorage.getItem('text')
+    alert('O texto salvo no local Storage é: ' + t)
+})
+
+document.getElementById('cookieBtn').addEventListener('click', function () {
+    const input = document.getElementById('cookie')
+    // cookieName=value; expires=UTCStringDate; path=/;
+    const cookie = 'info=' + input.value  + ';'
+    const expiration = 'expires=' + new Date(2023,9, 9) + ';'
+    const path = 'path=/;'
+    document.cookie = cookie + expiration + path 
+    input.value = ''
+    console.log(cookie)
+})
+
+document.getElementById('cookie2Btn').addEventListener('click', function () {
+    const input = document.getElementById('cookie2')
+    // cookieName=value; expires=UTCStringDate; path=/;
+    const cookie = 'text=' + input.value + ';'
+    const expiration = 'expires=' + new Date(2023,10, 9) + ';'
+    const path = 'path=/;'
+    document.cookie = cookie + expiration + path 
+    input.value = ''
+    console.log(cookie)
+})
+
