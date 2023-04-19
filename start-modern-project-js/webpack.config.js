dfe569f52bd7ae2e2ccfada995402998951f074a
Eduardo Andrade <109284750+eduardoandraade@users.noreply.github.com>
2023-04-19 13:26:27 -0300

    webpack-dev-server

--- start-modern-project-js/webpack.config.js
+++ start-modern-project-js/webpack.config.js
@@ -1,4 +1,13 @@
+const path = require('path')
+
 module.exports = {
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        compress: true,
+        port: 8000,
+    },
     entry: {
         index: './src/index.js'
     },
