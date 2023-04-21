655a505965429e9d3c2cbbec03831a5e29a0c3b9
Eduardo Andrade <109284750+eduardoandraade@users.noreply.github.com>
2023-04-21 11:16:09 -0300

    exercicio POO
    usando classes e o DOM

---
+++ objetos/exercicio-20/Component.js
@@ -0,0 +1,26 @@
+export class Component {
+    #element = null
+
+    constructor(tag, parent, options) {
+        this.tag = tag
+        this.parent = parent
+        this.options = options
+        this.build()
+    }
+
+    build() {
+        this.#element = document.createElement(this.tag)
+        Object.assign(this.#element, this.options)
+        return this
+    }
+
+    render() {
+        if (this.parent instanceof Component) {
+            this.parent.getElement().append(this.#element)
+        } else {
+            document.querySelector(this.parent).append(this.#element)
+        }
+    }
+}
+
+
